5557a8b7cfbc2fed837aa29a4d5ada0d5a732455
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2014-07-29 11:44:35 +0200

    Add code generate from riak_core rebar template

---
+++ simple_crawler/apps/sc/src/sc_app.erl
@@ -0,0 +1,25 @@
+-module(sc_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    case sc_sup:start_link() of
+        {ok, Pid} ->
+            ok = riak_core:register([{vnode_module, sc_vnode}]),
+            ok = riak_core_ring_events:add_guarded_handler(sc_ring_event_handler, []),
+            ok = riak_core_node_watcher_events:add_guarded_handler(sc_node_event_handler, []),
+            ok = riak_core_node_watcher:service_up(sc, self()),
+            {ok, Pid};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+stop(_State) ->
+    ok.
